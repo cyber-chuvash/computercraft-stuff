313f42a29ada42c354d23fac3f7ae3b8d3ae6102
Mikhail Migushov <m.migushov@gmail.com>
2020-12-06 21:11:13 +0300

    Add goHome func to test

--- turtle/mine.lua
+++ turtle/mine.lua
@@ -1,10 +1,10 @@
 
-function digAndMove(n)
+local function digAndMove(n)
     n = n or 1
     local i = 1
     while i <= n do
         turtle.dig()
-        didMove = turtle.forward()
+        local didMove = turtle.forward()
         if didMove then
             i = i + 1
         else
@@ -13,7 +13,25 @@ function digAndMove(n)
     end
 end
 
-function stripVerticalLayer(x, y)
+local function goHome(target_x, curr_x, curr_y, curr_z)
+    if curr_y % 2 == 0 then
+        turtle.turnLeft()
+    else
+        turtle.turnRight()
+    end
+    for _ = 1, curr_z do turtle.forward() end
+    for _ = 1, curr_y do turtle.down() end
+    turtle.turnRight()
+    local x_to_home
+    if curr_y % 2 == 1 then
+        x_to_home = curr_x
+    else
+        x_to_home = target_x - curr_x
+    end
+    for _ = 1, x_to_home do turtle.forward() end
+end
+
+local function stripVerticalLayer(x, y)
     digAndMove()
     turtle.turnRight()
     for y_pos = 1, y do
@@ -27,30 +45,31 @@ function stripVerticalLayer(x, y)
             turtle.turnRight()
         end
     end
-    for i = 1, y do turtle.down() end
+    for _ = 1, y do turtle.down() end
     if y % 2 ~= 0 then
         turtle.turnRight()
         turtle.turnRight()
-        for i = 1, x do turtle.forward() end
+        for _ = 1, x do turtle.forward() end
     end
     turtle.turnRight()
 end
 
-function mine(x, y, z)
+local function mine(x, y, z)
     for z_pos = 1, z do
         print(string.format('Striping vertical layer (%d/%d)', z_pos, z))
         stripVerticalLayer(x, y)
+        goHome(x, 1, 1, z_pos)
     end
     print('Returning home')
     turtle.turnRight()
     turtle.turnRight()
-    for i = 1, z do turtle.forward() end
+    for _ = 1, z do turtle.forward() end
     turtle.turnRight()
     turtle.turnRight()
     print('Done, halting.')
 end
 
-x, y, z = ...
+local x, y, z = ...
 x = tonumber(x)
 y = tonumber(y)
 z = tonumber(z)
